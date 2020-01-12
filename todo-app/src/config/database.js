34e74f13368b2ed364b277a28dcc388fcae6b9f6
gabriel-roque <gabrielrms.dll@gmail.com>
2020-01-11 23:36:18 -0300

    [ADD] API REST todo express

--- todo-app/src/config/database.js
+++ todo-app/src/config/database.js
@@ -1,7 +1,3 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose.connect("mongodb://localhost:27017/todo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/todo')
--- todo-app/src/config/server.js
+++ todo-app/src/config/server.js
@@ -1,12 +1,16 @@
-const PORT = 3003;
+const port = 3003
 
-const bodyParser = require("body-parser");
-const express = require("express");
-const server = express();
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
 
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
 
-server.listen(PORT, function() {
-  console.log(`API is running on port ${PORT}`);
-});
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
